e48ec3b2f5038bb5e4b111bd1857f3bb41bd3440
tcalhounjr <thomas@nafasi.co>
2018-03-23 23:22:09 -0400

    More UI changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,32 @@
 function getCopyrightDate() {
-    var copyright = document.getElementById('trivia-game-footer');
+    var copyright = document.getElementById('footer-text');
 
     var todaysDate = new Date();
     copyright.innerHTML = 'Copyright ' + todaysDate.getFullYear() + ' TCJR';
 }
+
+function buildGameBoard(gameArray) {
+    gameArray = shuffle(gameArray);
+    console.log('The gameArray has ',gameArray.length, ' elements');
+
+    for (var i = 0; i < gameArray.length; i++) {
+        var divHTML = $("<div class='radio'>");
+        var labelHTML = $("<label class='radio-label'>");
+        var radioHTML = $("<input type='radio' name='optionsRadios'>");
+        divHTML.appendTo(radioDIV);
+        labelHTML.appendTo(divHTML);
+        radioHTML.prependTo(divHTML);
+        
+        radioHTML.attr('id', 'optionsRadios' + gameElement);
+        radioHTML.attr('value', gameArray[i]);
+        labelHTML.text(gameArray[i]);
+    }
+}
+
+
+var giphyArray = ["KANYE", "JAY-Z", "BEYONCE", "RIHANNA", "DRAKE"];
+
+$("#fetch").on("click", function(event) {
+    event.preventDefault();
+    console.log('This is the giphy search array ', giphyArray);
+});
