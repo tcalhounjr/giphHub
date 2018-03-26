4ec3e037bfdeb092cb38f45ce1e7d5b718d0583d
tcalhounjr <thomas@nafasi.co>
2018-03-25 21:27:05 -0400

    Javascript is stable reading GIPHY ratings, API Key, and image limit from firebase

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -24,9 +24,51 @@ function buildGameBoard(gameArray) {
     }
 }
 
-const giphyDisplayHTML = "#giphy-display-submit";
+//INITIALIZE FIREBASE CONNECTION TO ACCESS 
+
+var config = {
+    apiKey: "AIzaSyCnPRe0G27hZE3Su-WIh43LjgMYaTqVaDc",
+    authDomain: "giphhub-hw.firebaseapp.com",
+    databaseURL: "https://giphhub-hw.firebaseio.com",
+    projectId: "giphhub-hw",
+    storageBucket: "giphhub-hw.appspot.com",
+    messagingSenderId: "473626731056"
+  };
+  firebase.initializeApp(config);
+
+//CREATE GLOBAL VARIABLES TO USE THROUGHOUT APP
+
+const giphyDisplayHTML = "#giphy-display-submit1";
+const imgHTML = $('<img>');
+const imgFormHTML = '#giphy-buttons';
+
 
 var giphyArray = ["KANYE", "JAY-Z", "BEYONCE", "RIHANNA", "DRAKE"];
+var database = firebase.database();
+var giphyObject = {};
+var giphyAPIKey = "";
+var giphyImgLimit = 0;
+var giphyRatingsArray = [];
+
+database.ref('giphyObject').on('value', function(snapshot) {
+    giphyObject = snapshot.val();
+    giphyAPIKey = giphyObject.apiKey;
+    giphyImgLimit = parseInt(giphyObject.imgLimit);
+    console.log(giphyAPIKey);
+    console.log(giphyImgLimit);
+});
+
+database.ref('giphyRatings').on('value', function(snapshot) {
+    
+    giphyRatingsArray = snapshot.val();
+    console.log(giphyRatingsArray);
+    
+    for (var i = 0; i < giphyRatingsArray.length; i++) {
+        console.log(giphyRatingsArray[i]);
+    }
+
+});
+
 
 $(giphyDisplayHTML).on("click", function(event) {
     event.preventDefault();
