87640cf514b6aa6d4a031e162a00a1caa886a35c
tcalhounjr <thomas@nafasi.co>
2018-03-26 05:05:10 -0400

    Starting the process of writing HTML to the screen

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,23 +5,35 @@ function getCopyrightDate() {
     copyright.innerHTML = 'Copyright ' + todaysDate.getFullYear() + ' TCJR';
 }
 
-function buildGameBoard(gameArray) {
-    gameArray = shuffle(gameArray);
-    console.log('The gameArray has ',gameArray.length, ' elements');
-
-    for (var i = 0; i < gameArray.length; i++) {
-        var divHTML = $("<div class='radio'>");
-        var labelHTML = $("<label class='radio-label'>");
-        var radioHTML = $("<input type='radio' name='optionsRadios'>");
-
-        divHTML.appendTo(radioDIV);
-        labelHTML.appendTo(divHTML);
-        radioHTML.prependTo(divHTML);
-        
-        radioHTML.attr('id', 'optionsRadios' + gameElement);
-        radioHTML.attr('value', gameArray[i]);
-        labelHTML.text(gameArray[i]);
+function writeToScreen(parentTag, childObject, array) {
+    console.log(array);
+    $(parentTag).empty();
+    for (var i = 0; i < array.length; i++) {
+        $(parentTag).append(childObject);
+        $(childObject).text(array[i]);
     }
+}
+
+function initializeGiphyValues (callback) {
+    
+    database.ref('giphyObject').on('value', function(snapshot) {
+        giphyObject = snapshot.val();
+        giphyAPIKey = giphyObject.apiKey;
+        giphyImgLimit = parseInt(giphyObject.imgLimit);
+        console.log(giphyAPIKey);
+        console.log(giphyImgLimit);
+    });
+    
+    database.ref('giphyRatings').on('value', function(snapshot) {
+        
+        giphyRatingsArray = snapshot.val();
+        console.log(giphyRatingsArray);
+        
+        for (var i = 0; i < giphyRatingsArray.length; i++) {
+            console.log(giphyRatingsArray[i]);
+        }
+    });
+    callback();
 }
 
 //INITIALIZE FIREBASE CONNECTION TO ACCESS 
@@ -38,39 +50,43 @@ var config = {
 
 //CREATE GLOBAL VARIABLES TO USE THROUGHOUT APP
 
+//jQuery objects
+const imgObject = $("<img class='giphy-images' src='assets/images/test/tcjr.jpg' title='Rated: Y'>");
+const buttonObject = $("<button type='submit' class='btn btn-primary giphy-button'>");
+const optionObject = $('<option>');
+
+//parent HTML tags for jQuery objects
+const btnFormHTML = '#giphy-buttons';
+const selectHTML = '.giphy-ratings';
+const divHTML = '.giphy-display-buttons';
+
+//get HTML tag info for event listeners
 const giphyDisplayHTML = "#giphy-display-submit1";
-const imgHTML = $('<img>');
-const imgFormHTML = '#giphy-buttons';
+const searchBtnHTML = '.search-button';
+const textBoxHTML = '#search-text';
 
-
-var giphyArray = ["KANYE", "JAY-Z", "BEYONCE", "RIHANNA", "DRAKE"];
 var database = firebase.database();
+var giphyButtonsArray = ["KANYE", "JAY-Z", "BEYONCE", "RIHANNA", "DRAKE"];
 var giphyObject = {};
 var giphyAPIKey = "";
 var giphyImgLimit = 0;
 var giphyRatingsArray = [];
 
-database.ref('giphyObject').on('value', function(snapshot) {
-    giphyObject = snapshot.val();
-    giphyAPIKey = giphyObject.apiKey;
-    giphyImgLimit = parseInt(giphyObject.imgLimit);
-    console.log(giphyAPIKey);
-    console.log(giphyImgLimit);
-});
-
-database.ref('giphyRatings').on('value', function(snapshot) {
+$(document).ready(function() {
     
-    giphyRatingsArray = snapshot.val();
-    console.log(giphyRatingsArray);
-    
-    for (var i = 0; i < giphyRatingsArray.length; i++) {
-        console.log(giphyRatingsArray[i]);
-    }
+    initializeGiphyValues(function () {
+        writeToScreen(selectHTML,optionObject,giphyRatingsArray);
+    });
 
-});
+    $(searchBtnHTML).on("click", function(event) {
+        event.preventDefault();
+        console.log('This is the giphy search array ', $(textBoxHTML).val());
 
+    });
 
-$(giphyDisplayHTML).on("click", function(event) {
-    event.preventDefault();
-    console.log('This is the giphy search array ', giphyArray);
+    $(giphyDisplayHTML).on("click", function(event) {
+        event.preventDefault();
+        console.log('This is the giphy search array ', giphyArray);
+    });
+
 });
